46b5f592b941525520ec6db6403f2a5a533aa856
YGX-TOUCH-FISH <1816303037@qq.com>
2022-01-18 21:43:47 +0800

    oj test4

--- src/Isaiah.ll
+++ src/Isaiah.ll
@@ -19,81 +19,162 @@ declare i8* @getString()
 declare i32 @getInt()
 declare i8* @toString(i32)
 
-@n = global i32 0
-@r = global i32 0
-@c = global i32 0
-@i = global i32 0
-@j = global i32 0
+@.str.0 = constant [2 x i8] c" \00"
+@.str.1 = constant [1 x i8] c"\00"
 
-define i32 @abs(i32 %0){
-	%2 = alloca i32
-	store i32 %0, i32* %2
-	%3 = load i32, i32* %2
-	%4 = icmp sgt i32 %3, 0
-	%5 = icmp ne i1 %4, 0
-	br i1 %5, label %6, label %8
-6:
-	%7 = load i32, i32* %2
-	ret i32 %7
-8:
-	%9 = load i32, i32* %2
-	%10 = sub i32 0, %9
-	ret i32 %10
+define void @bubble_sort(i32* %0){
+	%2 = alloca i32*
+	store i32* %0, i32** %2
+	%3 = alloca i32
+	%4 = alloca i32
+	%5 = load i32*, i32** %2
+	%6 = bitcast i32* %5 to i8*
+	%7 = call i32 @array_size(i8* %6)
+	%8 = alloca i32
+	store i32 %7, i32* %8
+	store i32 0, i32* %3
+	br label %9
+9:
+	%10 = load i32, i32* %8
+	%11 = sub i32 %10, 1
+	%12 = load i32, i32* %3
+	%13 = icmp slt i32 %12, %11
+	%14 = icmp ne i1 %13, 0
+	br i1 %14, label %15, label %63
+15:
+	store i32 0, i32* %4
+	br label %16
+16:
+	%17 = load i32, i32* %8
+	%18 = load i32, i32* %3
+	%19 = sub i32 %17, %18
+	%20 = sub i32 %19, 1
+	%21 = load i32, i32* %4
+	%22 = icmp slt i32 %21, %20
+	%23 = icmp ne i1 %22, 0
+	br i1 %23, label %24, label %59
+24:
+	%25 = load i32, i32* %4
+	%26 = load i32*, i32** %2
+	%27 = getelementptr inbounds i32, i32* %26, i32 %25
+	%28 = load i32, i32* %4
+	%29 = add i32 %28, 1
+	%30 = load i32*, i32** %2
+	%31 = getelementptr inbounds i32, i32* %30, i32 %29
+	%32 = load i32, i32* %27
+	%33 = load i32, i32* %31
+	%34 = icmp slt i32 %32, %33
+	%35 = icmp ne i1 %34, 0
+	br i1 %35, label %36, label %37
+36:
+	br label %56
+37:
+	%38 = load i32, i32* %4
+	%39 = load i32*, i32** %2
+	%40 = getelementptr inbounds i32, i32* %39, i32 %38
+	%41 = load i32, i32* %40
+	%42 = alloca i32
+	store i32 %41, i32* %42
+	%43 = load i32, i32* %4
+	%44 = load i32*, i32** %2
+	%45 = getelementptr inbounds i32, i32* %44, i32 %43
+	%46 = load i32, i32* %4
+	%47 = add i32 %46, 1
+	%48 = load i32*, i32** %2
+	%49 = getelementptr inbounds i32, i32* %48, i32 %47
+	%50 = load i32, i32* %49
+	store i32 %50, i32* %45
+	%51 = load i32, i32* %4
+	%52 = add i32 %51, 1
+	%53 = load i32*, i32** %2
+	%54 = getelementptr inbounds i32, i32* %53, i32 %52
+	%55 = load i32, i32* %42
+	store i32 %55, i32* %54
+	br label %56
+56:
+	%57 = load i32, i32* %4
+	%58 = add i32 %57, 1
+	store i32 %58, i32* %4
+	br label %16
+59:
+	br label %60
+60:
+	%61 = load i32, i32* %3
+	%62 = add i32 %61, 1
+	store i32 %62, i32* %3
+	br label %9
+63:
+	ret void
 }
 
 define i32 @main(){
 	call void @global_var_init()
-	store i32 0, i32* @i
-	br label %1
-1:
-	%2 = load i32, i32* @i
-	%3 = icmp slt i32 %2, 5
-	%4 = icmp ne i1 %3, 0
-	br i1 %4, label %5, label %26
-5:
-	store i32 0, i32* @j
-	br label %6
-6:
-	%7 = load i32, i32* @j
-	%8 = icmp slt i32 %7, 5
-	%9 = icmp ne i1 %8, 0
-	br i1 %9, label %10, label %22
-10:
-	%11 = call i32 @getInt()
-	store i32 %11, i32* @n
-	%12 = load i32, i32* @n
-	%13 = icmp eq i32 %12, 1
-	%14 = icmp ne i1 %13, 0
-	br i1 %14, label %15, label %18
-15:
-	%16 = load i32, i32* @i
-	store i32 %16, i32* @r
-	%17 = load i32, i32* @j
-	store i32 %17, i32* @c
-	br label %18
-18:
-	br label %19
+	%1 = call i32 @getInt()
+	%2 = alloca i32
+	store i32 %1, i32* %2
+	%3 = load i32, i32* %2
+	%4 = mul i32 %3, 4
+	%5 = add i32 %4, 4
+	%6 = call i8* @malloc(i32 %5)
+	%7 = bitcast i8* %6 to i32*
+	store i32 %3, i32* %7
+	%8 = getelementptr inbounds i8, i8* %6, i32 4
+	%9 = bitcast i8* %8 to i32*
+	%10 = alloca i32*
+	store i32* %9, i32** %10
+	%11 = load i32*, i32** %10
+	%12 = alloca i32*
+	store i32* %11, i32** %12
+	%13 = alloca i32
+	store i32 0, i32* %13
+	br label %14
+14:
+	%15 = load i32, i32* %13
+	%16 = load i32, i32* %2
+	%17 = icmp slt i32 %15, %16
+	%18 = icmp ne i1 %17, 0
+	br i1 %18, label %19, label %27
 19:
-	%20 = load i32, i32* @j
-	%21 = add i32 %20, 1
-	store i32 %21, i32* @j
-	br label %6
-22:
-	br label %23
-23:
-	%24 = load i32, i32* @i
-	%25 = add i32 %24, 1
-	store i32 %25, i32* @i
-	br label %1
-26:
-	%27 = load i32, i32* @r
-	%28 = sub i32 2, %27
-	%29 = call i32 @abs(i32 %28)
-	%30 = load i32, i32* @c
-	%31 = sub i32 2, %30
-	%32 = call i32 @abs(i32 %31)
-	%33 = add i32 %29, %32
-	call void @printInt(i32 %33)
+	%20 = load i32, i32* %13
+	%21 = load i32*, i32** %12
+	%22 = getelementptr inbounds i32, i32* %21, i32 %20
+	%23 = call i32 @getInt()
+	store i32 %23, i32* %22
+	br label %24
+24:
+	%25 = load i32, i32* %13
+	%26 = add i32 %25, 1
+	store i32 %26, i32* %13
+	br label %14
+27:
+	%28 = load i32*, i32** %12
+	call void @bubble_sort(i32* %28)
+	store i32 0, i32* %13
+	br label %29
+29:
+	%30 = load i32, i32* %13
+	%31 = load i32, i32* %2
+	%32 = icmp slt i32 %30, %31
+	%33 = icmp ne i1 %32, 0
+	br i1 %33, label %34, label %45
+34:
+	%35 = load i32, i32* %13
+	%36 = load i32*, i32** %12
+	%37 = getelementptr inbounds i32, i32* %36, i32 %35
+	%38 = load i32, i32* %37
+	%39 = call i8* @toString(i32 %38)
+	%40 = bitcast [2 x i8]* @.str.0 to i8*
+	%41 = call i8* @string_add(i8* %39, i8* %40)
+	call void @print(i8* %41)
+	br label %42
+42:
+	%43 = load i32, i32* %13
+	%44 = add i32 %43, 1
+	store i32 %44, i32* %13
+	br label %29
+45:
+	%46 = bitcast [1 x i8]* @.str.1 to i8*
+	call void @println(i8* %46)
 	ret i32 0
 }
 
--- src/out/production/3CompilerWorkplace/output.s
+++ src/out/production/3CompilerWorkplace/output.s
@@ -1,155 +1,409 @@
 	.text
-	.globl	abs
+	.globl	bubble_sort
 	.p2align	2
-	.type	abs,@function
-abs:
-	addi	sp, sp, -96
-	sw	ra, 92(sp)
-	sw	s0, 88(sp)
-	sw	s1, 84(sp)
-	sw	s2, 80(sp)
-	sw	s3, 76(sp)
-	sw	s4, 72(sp)
-	sw	s5, 68(sp)
-	sw	s6, 64(sp)
-	sw	s7, 60(sp)
-	sw	s8, 56(sp)
-	sw	s9, 52(sp)
-	sw	s10, 48(sp)
-	sw	s11, 44(sp)
-	sw	a0, 40(sp)
-	lw	s0, 40(sp)
-	sw	s0, 36(sp)
-	lw	s0, 36(sp)
-	sw	s0, 32(sp)
-	lw	s0, 32(sp)
-	li	s1, 0
+	.type	bubble_sort,@function
+bubble_sort:
+	addi	sp, sp, -272
+	sw	ra, 268(sp)
+	sw	s0, 264(sp)
+	sw	s1, 260(sp)
+	sw	s2, 256(sp)
+	sw	s3, 252(sp)
+	sw	s4, 248(sp)
+	sw	s5, 244(sp)
+	sw	s6, 240(sp)
+	sw	s7, 236(sp)
+	sw	s8, 232(sp)
+	sw	s9, 228(sp)
+	sw	s10, 224(sp)
+	sw	s11, 220(sp)
+	sw	a0, 216(sp)
+	lw	s0, 216(sp)
+	sw	s0, 212(sp)
+	lw	s0, 212(sp)
+	sw	s0, 200(sp)
+	lw	s0, 200(sp)
+	sw	s0, 196(sp)
+	lw	a0, 196(sp)
+	call	array_size
+	sw	a0, 192(sp)
+	lw	s0, 192(sp)
+	sw	s0, 188(sp)
+	li	s0, 0
+	sw	s0, 208(sp)
+	j	.bubble_sort_1
+.bubble_sort_1:
+	lw	s0, 188(sp)
+	sw	s0, 184(sp)
+	lw	s0, 184(sp)
+	li	s1, 1
 	sub	s2, s0,s1
-	sgtz	s4, s2
-	sw	s4, 28(sp)
-	lw	s0, 28(sp)
+	sw	s2, 180(sp)
+	lw	s0, 208(sp)
+	sw	s0, 176(sp)
+	lw	s0, 176(sp)
+	lw	s1, 180(sp)
+	sub	s2, s0,s1
+	sltz	s4, s2
+	sw	s4, 172(sp)
+	lw	s0, 172(sp)
 	li	s1, 0
 	sub	s2, s0,s1
 	snez	s4, s2
-	sw	s4, 24(sp)
-	lw	s0, 24(sp)
-	beqz s0 .abs_2
-.abs_1:
-	lw	s0, 36(sp)
-	sw	s0, 20(sp)
-	lw	a0, 20(sp)
-	lw	s0, 88(sp)
-	lw	s1, 84(sp)
-	lw	s2, 80(sp)
-	lw	s3, 76(sp)
-	lw	s4, 72(sp)
-	lw	s5, 68(sp)
-	lw	s6, 64(sp)
-	lw	s7, 60(sp)
-	lw	s8, 56(sp)
-	lw	s9, 52(sp)
-	lw	s10, 48(sp)
-	lw	s11, 44(sp)
-	lw	ra, 92(sp)
-	addi	sp, sp, 96
-	ret
-.abs_2:
-	lw	s0, 36(sp)
-	sw	s0, 16(sp)
+	sw	s4, 168(sp)
+	lw	s0, 168(sp)
+	beqz s0, .bubble_sort_10
+.bubble_sort_2:
 	li	s0, 0
-	lw	s1, 16(sp)
+	sw	s0, 204(sp)
+	j	.bubble_sort_3
+.bubble_sort_3:
+	lw	s0, 188(sp)
+	sw	s0, 164(sp)
+	lw	s0, 208(sp)
+	sw	s0, 160(sp)
+	lw	s0, 164(sp)
+	lw	s1, 160(sp)
 	sub	s2, s0,s1
-	sw	s2, 12(sp)
-	lw	a0, 12(sp)
-	lw	s0, 88(sp)
+	sw	s2, 156(sp)
+	lw	s0, 156(sp)
+	li	s1, 1
+	sub	s2, s0,s1
+	sw	s2, 152(sp)
+	lw	s0, 204(sp)
+	sw	s0, 148(sp)
+	lw	s0, 148(sp)
+	lw	s1, 152(sp)
+	sub	s2, s0,s1
+	sltz	s4, s2
+	sw	s4, 144(sp)
+	lw	s0, 144(sp)
+	li	s1, 0
+	sub	s2, s0,s1
+	snez	s4, s2
+	sw	s4, 140(sp)
+	lw	s0, 140(sp)
+	beqz s0, .bubble_sort_8
+.bubble_sort_4:
+	lw	s0, 204(sp)
+	sw	s0, 136(sp)
+	lw	s0, 212(sp)
+	sw	s0, 132(sp)
+	lw	s1, 132(sp)
+	lw	s2, 136(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 128(sp)
+	lw	s0, 204(sp)
+	sw	s0, 124(sp)
+	lw	s0, 124(sp)
+	li	s1, 1
+	add	s2, s0,s1
+	sw	s2, 120(sp)
+	lw	s0, 212(sp)
+	sw	s0, 116(sp)
+	lw	s1, 116(sp)
+	lw	s2, 120(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 112(sp)
+	lw	s1, 128(sp)
+	lw	s0, 0(s1)
+	sw	s0, 108(sp)
+	lw	s1, 112(sp)
+	lw	s0, 0(s1)
+	sw	s0, 104(sp)
+	lw	s0, 108(sp)
+	lw	s1, 104(sp)
+	sub	s2, s0,s1
+	sltz	s4, s2
+	sw	s4, 100(sp)
+	lw	s0, 100(sp)
+	li	s1, 0
+	sub	s2, s0,s1
+	snez	s4, s2
+	sw	s4, 96(sp)
+	lw	s0, 96(sp)
+	beqz s0, .bubble_sort_6
+.bubble_sort_5:
+	j	.bubble_sort_7
+.bubble_sort_6:
+	lw	s0, 204(sp)
+	sw	s0, 92(sp)
+	lw	s0, 212(sp)
+	sw	s0, 88(sp)
+	lw	s1, 88(sp)
+	lw	s2, 92(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 84(sp)
 	lw	s1, 84(sp)
-	lw	s2, 80(sp)
-	lw	s3, 76(sp)
-	lw	s4, 72(sp)
-	lw	s5, 68(sp)
-	lw	s6, 64(sp)
-	lw	s7, 60(sp)
-	lw	s8, 56(sp)
-	lw	s9, 52(sp)
-	lw	s10, 48(sp)
-	lw	s11, 44(sp)
-	lw	ra, 92(sp)
-	addi	sp, sp, 96
+	lw	s0, 0(s1)
+	sw	s0, 80(sp)
+	lw	s0, 80(sp)
+	sw	s0, 76(sp)
+	lw	s0, 204(sp)
+	sw	s0, 72(sp)
+	lw	s0, 212(sp)
+	sw	s0, 68(sp)
+	lw	s1, 68(sp)
+	lw	s2, 72(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 64(sp)
+	lw	s0, 204(sp)
+	sw	s0, 60(sp)
+	lw	s0, 60(sp)
+	li	s1, 1
+	add	s2, s0,s1
+	sw	s2, 56(sp)
+	lw	s0, 212(sp)
+	sw	s0, 52(sp)
+	lw	s1, 52(sp)
+	lw	s2, 56(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 48(sp)
+	lw	s1, 48(sp)
+	lw	s0, 0(s1)
+	sw	s0, 44(sp)
+	lw	s0, 44(sp)
+	lw	s1, 64(sp)
+	sw	s0, 0(s1)
+	lw	s0, 204(sp)
+	sw	s0, 40(sp)
+	lw	s0, 40(sp)
+	li	s1, 1
+	add	s2, s0,s1
+	sw	s2, 36(sp)
+	lw	s0, 212(sp)
+	sw	s0, 32(sp)
+	lw	s1, 32(sp)
+	lw	s2, 36(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 28(sp)
+	lw	s0, 76(sp)
+	sw	s0, 24(sp)
+	lw	s0, 24(sp)
+	lw	s1, 28(sp)
+	sw	s0, 0(s1)
+	j	.bubble_sort_7
+.bubble_sort_7:
+	lw	s0, 204(sp)
+	sw	s0, 20(sp)
+	lw	s0, 20(sp)
+	li	s1, 1
+	add	s2, s0,s1
+	sw	s2, 16(sp)
+	lw	s0, 16(sp)
+	sw	s0, 204(sp)
+	j	.bubble_sort_3
+.bubble_sort_8:
+	j	.bubble_sort_9
+.bubble_sort_9:
+	lw	s0, 208(sp)
+	sw	s0, 12(sp)
+	lw	s0, 12(sp)
+	li	s1, 1
+	add	s2, s0,s1
+	sw	s2, 8(sp)
+	lw	s0, 8(sp)
+	sw	s0, 208(sp)
+	j	.bubble_sort_1
+.bubble_sort_10:
+	lw	s0, 264(sp)
+	lw	s1, 260(sp)
+	lw	s2, 256(sp)
+	lw	s3, 252(sp)
+	lw	s4, 248(sp)
+	lw	s5, 244(sp)
+	lw	s6, 240(sp)
+	lw	s7, 236(sp)
+	lw	s8, 232(sp)
+	lw	s9, 228(sp)
+	lw	s10, 224(sp)
+	lw	s11, 220(sp)
+	lw	ra, 268(sp)
+	addi	sp, sp, 272
 	ret
 
 	.globl	main
 	.p2align	2
 	.type	main,@function
 main:
-	addi	sp, sp, -144
-	sw	ra, 140(sp)
-	sw	s0, 136(sp)
-	sw	s1, 132(sp)
-	sw	s2, 128(sp)
-	sw	s3, 124(sp)
-	sw	s4, 120(sp)
-	sw	s5, 116(sp)
-	sw	s6, 112(sp)
-	sw	s7, 108(sp)
-	sw	s8, 104(sp)
-	sw	s9, 100(sp)
-	sw	s10, 96(sp)
-	sw	s11, 92(sp)
+	addi	sp, sp, -272
+	sw	ra, 268(sp)
+	sw	s0, 264(sp)
+	sw	s1, 260(sp)
+	sw	s2, 256(sp)
+	sw	s3, 252(sp)
+	sw	s4, 248(sp)
+	sw	s5, 244(sp)
+	sw	s6, 240(sp)
+	sw	s7, 236(sp)
+	sw	s8, 232(sp)
+	sw	s9, 228(sp)
+	sw	s10, 224(sp)
+	sw	s11, 220(sp)
 	call	global_var_init
-	li	s0, 0
-	la	s1, i
+	call	getInt
+	sw	a0, 216(sp)
+	lw	s0, 216(sp)
+	sw	s0, 212(sp)
+	lw	s0, 212(sp)
+	sw	s0, 208(sp)
+	lw	s0, 208(sp)
+	li	s1, 4
+	mul	s2, s0,s1
+	sw	s2, 204(sp)
+	lw	s0, 204(sp)
+	li	s1, 4
+	add	s2, s0,s1
+	sw	s2, 200(sp)
+	lw	a0, 200(sp)
+	call	malloc
+	sw	a0, 196(sp)
+	lw	s0, 196(sp)
+	sw	s0, 192(sp)
+	lw	s0, 208(sp)
+	lw	s1, 192(sp)
 	sw	s0, 0(s1)
+	lw	s1, 196(sp)
+	li	s2, 4
+	add	s0, s1,s2
+	sw	s0, 188(sp)
+	lw	s0, 188(sp)
+	sw	s0, 184(sp)
+	lw	s0, 184(sp)
+	sw	s0, 180(sp)
+	lw	s0, 180(sp)
+	sw	s0, 176(sp)
+	lw	s0, 176(sp)
+	sw	s0, 172(sp)
+	li	s0, 0
+	sw	s0, 168(sp)
 	j	.main_1
 .main_1:
-	la	s1, i
-	lw	s0, 0(s1)
-	sw	s0, 88(sp)
-	lw	s0, 88(sp)
-	li	s1, 5
+	lw	s0, 168(sp)
+	sw	s0, 164(sp)
+	lw	s0, 212(sp)
+	sw	s0, 160(sp)
+	lw	s0, 164(sp)
+	lw	s1, 160(sp)
 	sub	s2, s0,s1
 	sltz	s4, s2
-	sw	s4, 84(sp)
-	lw	s0, 84(sp)
+	sw	s4, 156(sp)
+	lw	s0, 156(sp)
 	li	s1, 0
 	sub	s2, s0,s1
 	snez	s4, s2
-	sw	s4, 80(sp)
-	lw	s0, 80(sp)
-	beqz s0 .main_10
+	sw	s4, 152(sp)
+	lw	s0, 152(sp)
+	beqz s0, .main_4
 .main_2:
-	li	s0, 0
-	la	s1, j
+	lw	s0, 168(sp)
+	sw	s0, 148(sp)
+	lw	s0, 172(sp)
+	sw	s0, 144(sp)
+	lw	s1, 144(sp)
+	lw	s2, 148(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 140(sp)
+	call	getInt
+	sw	a0, 136(sp)
+	lw	s0, 136(sp)
+	lw	s1, 140(sp)
 	sw	s0, 0(s1)
 	j	.main_3
 .main_3:
-	la	s1, j
-	lw	s0, 0(s1)
-	sw	s0, 76(sp)
-	lw	s0, 76(sp)
-	li	s1, 5
+	lw	s0, 168(sp)
+	sw	s0, 132(sp)
+	lw	s0, 132(sp)
+	li	s1, 1
+	add	s2, s0,s1
+	sw	s2, 128(sp)
+	lw	s0, 128(sp)
+	sw	s0, 168(sp)
+	j	.main_1
+.main_4:
+	li	s0, 0
+	sw	s0, 168(sp)
+	j	.main_5
+.main_5:
+	lw	s0, 168(sp)
+	sw	s0, 124(sp)
+	lw	s0, 212(sp)
+	sw	s0, 120(sp)
+	lw	s0, 124(sp)
+	lw	s1, 120(sp)
 	sub	s2, s0,s1
 	sltz	s4, s2
-	sw	s4, 72(sp)
-	lw	s0, 72(sp)
+	sw	s4, 116(sp)
+	lw	s0, 116(sp)
 	li	s1, 0
 	sub	s2, s0,s1
 	snez	s4, s2
-	sw	s4, 68(sp)
-	lw	s0, 68(sp)
-	beqz s0 .main_8
-.main_4:
-	call	getInt
-	sw	a0, 64(sp)
-	lw	s0, 64(sp)
-	la	s1, n
-	sw	s0, 0(s1)
-	la	s1, n
+	sw	s4, 112(sp)
+	lw	s0, 112(sp)
+	beqz s0, .main_8
+.main_6:
+	lw	s0, 168(sp)
+	sw	s0, 108(sp)
+	lw	s0, 172(sp)
+	sw	s0, 104(sp)
+	lw	s1, 104(sp)
+	lw	s2, 108(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 100(sp)
+	lw	s1, 100(sp)
 	lw	s0, 0(s1)
-	sw	s0, 60(sp)
-	lw	s0, 60(sp)
+	sw	s0, 96(sp)
+	lw	a0, 96(sp)
+	call	toString
+	sw	a0, 92(sp)
+	la	s0, .str.0
+	sw	s0, 88(sp)
+	lw	a0, 92(sp)
+	lw	a1, 88(sp)
+	call	string_add
+	sw	a0, 84(sp)
+	lw	a0, 84(sp)
+	call	print
+	j	.main_7
+.main_7:
+	lw	s0, 168(sp)
+	sw	s0, 80(sp)
+	lw	s0, 80(sp)
 	li	s1, 1
+	add	s2, s0,s1
+	sw	s2, 76(sp)
+	lw	s0, 76(sp)
+	sw	s0, 168(sp)
+	j	.main_5
+.main_8:
+	la	s0, .str.1
+	sw	s0, 72(sp)
+	lw	a0, 72(sp)
+	call	println
+	lw	s0, 172(sp)
+	sw	s0, 68(sp)
+	lw	a0, 68(sp)
+	call	bubble_sort
+	li	s0, 0
+	sw	s0, 168(sp)
+	j	.main_9
+.main_9:
+	lw	s0, 168(sp)
+	sw	s0, 64(sp)
+	lw	s0, 212(sp)
+	sw	s0, 60(sp)
+	lw	s0, 64(sp)
+	lw	s1, 60(sp)
 	sub	s2, s0,s1
-	seqz	s4, s2
+	sltz	s4, s2
 	sw	s4, 56(sp)
 	lw	s0, 56(sp)
 	li	s1, 0
@@ -157,91 +411,62 @@ main:
 	snez	s4, s2
 	sw	s4, 52(sp)
 	lw	s0, 52(sp)
-	beqz s0 .main_6
-.main_5:
-	la	s1, i
-	lw	s0, 0(s1)
-	sw	s0, 48(sp)
-	lw	s0, 48(sp)
-	la	s1, r
-	sw	s0, 0(s1)
-	la	s1, j
-	lw	s0, 0(s1)
-	sw	s0, 44(sp)
-	lw	s0, 44(sp)
-	la	s1, c
-	sw	s0, 0(s1)
-	j	.main_6
-.main_6:
-	j	.main_7
-.main_7:
-	la	s1, j
-	lw	s0, 0(s1)
-	sw	s0, 40(sp)
-	lw	s0, 40(sp)
-	li	s1, 1
-	add	s2, s0,s1
-	sw	s2, 36(sp)
-	lw	s0, 36(sp)
-	la	s1, j
-	sw	s0, 0(s1)
-	j	.main_3
-.main_8:
-	j	.main_9
-.main_9:
-	la	s1, i
-	lw	s0, 0(s1)
-	sw	s0, 32(sp)
-	lw	s0, 32(sp)
-	li	s1, 1
-	add	s2, s0,s1
-	sw	s2, 28(sp)
-	lw	s0, 28(sp)
-	la	s1, i
-	sw	s0, 0(s1)
-	j	.main_1
+	beqz s0, .main_12
 .main_10:
-	la	s1, r
+	lw	s0, 168(sp)
+	sw	s0, 48(sp)
+	lw	s0, 172(sp)
+	sw	s0, 44(sp)
+	lw	s1, 44(sp)
+	lw	s2, 48(sp)
+	slli	s3, s2, 2
+	add	s0, s1,s3
+	sw	s0, 40(sp)
+	lw	s1, 40(sp)
 	lw	s0, 0(s1)
-	sw	s0, 24(sp)
-	li	s0, 2
-	lw	s1, 24(sp)
-	sub	s2, s0,s1
-	sw	s2, 20(sp)
-	lw	a0, 20(sp)
-	call	abs
-	sw	a0, 16(sp)
-	la	s1, c
-	lw	s0, 0(s1)
-	sw	s0, 12(sp)
-	li	s0, 2
-	lw	s1, 12(sp)
-	sub	s2, s0,s1
-	sw	s2, 8(sp)
-	lw	a0, 8(sp)
-	call	abs
-	sw	a0, 4(sp)
-	lw	s0, 16(sp)
-	lw	s1, 4(sp)
+	sw	s0, 36(sp)
+	lw	a0, 36(sp)
+	call	toString
+	sw	a0, 32(sp)
+	la	s0, .str.2
+	sw	s0, 28(sp)
+	lw	a0, 32(sp)
+	lw	a1, 28(sp)
+	call	string_add
+	sw	a0, 24(sp)
+	lw	a0, 24(sp)
+	call	print
+	j	.main_11
+.main_11:
+	lw	s0, 168(sp)
+	sw	s0, 20(sp)
+	lw	s0, 20(sp)
+	li	s1, 1
 	add	s2, s0,s1
-	sw	s2, 0(sp)
-	lw	a0, 0(sp)
-	call	printInt
+	sw	s2, 16(sp)
+	lw	s0, 16(sp)
+	sw	s0, 168(sp)
+	j	.main_9
+.main_12:
+	la	s0, .str.3
+	sw	s0, 12(sp)
+	lw	a0, 12(sp)
+	call	println
 	li	a0, 0
-	lw	s0, 136(sp)
-	lw	s1, 132(sp)
-	lw	s2, 128(sp)
-	lw	s3, 124(sp)
-	lw	s4, 120(sp)
-	lw	s5, 116(sp)
-	lw	s6, 112(sp)
-	lw	s7, 108(sp)
-	lw	s8, 104(sp)
-	lw	s9, 100(sp)
-	lw	s10, 96(sp)
-	lw	s11, 92(sp)
-	lw	ra, 140(sp)
-	addi	sp, sp, 144
+	lw	s0, 264(sp)
+	lw	s1, 260(sp)
+	lw	s2, 256(sp)
+	lw	s3, 252(sp)
+	lw	s4, 248(sp)
+	lw	s5, 244(sp)
+	lw	s6, 240(sp)
+	lw	s7, 236(sp)
+	lw	s8, 232(sp)
+	lw	s9, 228(sp)
+	lw	s10, 224(sp)
+	lw	s11, 220(sp)
+	lw	ra, 268(sp)
+	addi	sp, sp, 272
 	ret
 
 	.globl	global_var_init
@@ -280,36 +505,30 @@ global_var_init:
 
 
 	.section	.bss,"aw",@nobits
-	.type	n,@object
-	.globl	n
-n:
-	.word	0
-	.size	n, 4
-
-	.type	r,@object
-	.globl	r
-r:
-	.word	0
-	.size	r, 4
-
-	.type	c,@object
-	.globl	c
-c:
-	.word	0
-	.size	c, 4
-
-	.type	i,@object
-	.globl	i
-i:
-	.word	0
-	.size	i, 4
-
-	.type	j,@object
-	.globl	j
-j:
-	.word	0
-	.size	j, 4
-
 
 	.section	.rodata,"a",@progbits
+	.type	.str.0,@object
+	.globl	.str.0
+.str.0: 
+	.asciz	" \00"
+	.size	.str.0, 7
+
+	.type	.str.1,@object
+	.globl	.str.1
+.str.1: 
+	.asciz	"\00"
+	.size	.str.1, 6
+
+	.type	.str.2,@object
+	.globl	.str.2
+.str.2: 
+	.asciz	" \00"
+	.size	.str.2, 7
+
+	.type	.str.3,@object
+	.globl	.str.3
+.str.3: 
+	.asciz	"\00"
+	.size	.str.3, 6
+
 	.section	".note.GNU-stack","",@progbits
